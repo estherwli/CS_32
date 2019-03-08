7aca61a0a1551d3b1b9a7bc3b0c177ed0d629652
estherwli <li.esther2000@gmail.com>
2019-03-07 23:56:09 -0800

    first draft of trie insert function

--- Project4_EstherLi/Project4_EstherLi/Trie.h
+++ Project4_EstherLi/Project4_EstherLi/Trie.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <iostream>
+
 
 template<typename ValueType>
 class Trie
@@ -14,13 +16,15 @@ public:
 	~Trie();
 	void reset();
 	void insert(const std::string& key, const ValueType& value);
-	std::vector<ValueType> find(const std::string& key, bool exactMatchOnly) const;
+	//std::vector<ValueType> find(const std::string& key, bool exactMatchOnly) const;
+
 
 	// C++11 syntax for preventing copying and assignment
 	Trie(const Trie&) = delete;
 	Trie& operator=(const Trie&) = delete;
 
 private:
+	struct Node;
 	struct Pair {
 		Pair(char label, Node* child) {
 			m_label = label;
@@ -28,32 +32,36 @@ private:
 		}
 		~Pair() {} //DO I NEED TO IMPLEMENT THIS LATER?
 
+
+
 		//accessor functions
 		char label() { return m_label; }
 		Node* child() { return m_child; }
 
-		//private data members for Pair
+		//data members for Pair
 		char m_label;
 		Node* m_child;
 	};
+
 	struct Node {
 		Node() {}
 		Node(ValueType value) { m_values.push_back(value); }
 		Node(Pair child) { m_children.push_back(child); }
 		~Node() {
-			for (int i = m_values.end(); i >= 0; i--) //destructs the m_values vector 
-				m_values.pop_back();
-			Pair* p = &m_children.front(); //destructs the m_children list
-			while (p != nullptr) {
-				Node *n = p->next;
-				delete p;
-				p = n;
-			}
+			m_children.clear();
+			//for (int i = m_values.size(); i >= 0; i--) //destructs the m_values vector 
+			//	m_values.pop_back();
+			//Node* p = &m_children.front(); //destructs the m_children list
+			//while (p != nullptr) {
+			//	Node* n = p->next;
+			//	delete p;
+			//	p = n;
+			
 		}
 
-		//private data members for Node 
-		vector<ValueType> m_values;
-		list<Pair> m_children;
+		//data members for Node 
+		std::vector<ValueType> m_values;
+		std::list<Pair> m_children;
 	};
 
 	//private functions for Trie
@@ -61,6 +69,12 @@ private:
 
 	//private data member for Trie 
 	Node* m_root;
+
+//public:
+//	//************************************
+//	void printMe(Node* current);
+//	void print();
+//	//************************************
 };
 
 template<typename ValueType>
@@ -77,8 +91,13 @@ template<typename ValueType>
 void Trie<ValueType>::freeTree(Node* current) {
 	if (current == nullptr)
 		return;
-	freeTree(current->left);
-	freeTree(current->right);
+	if (current->m_children.size() != 0) {
+		Pair* p = &(current->m_children.front());
+		while (p != nullptr) {
+			freeTree(p->child());
+			p++;
+		}
+	}
 	delete current;
 }
 
@@ -91,45 +110,67 @@ void Trie<ValueType>::reset() {
 template<typename ValueType>
 void Trie<ValueType>::insert(const std::string& key, const ValueType& value) {
 	int i = 0;
-	Pair* j = m_root->m_children.front();
-	Node* prev;
-	while (i < key.size() && j != nullptr) {
-		if (j->label() == key[i]) { //found a Node with the right label, so move on to the next character 
-			i++;
-			if (i == key.size()) {//found the last character of key
-				j->child()->m_values.push_back(value); //push value to the m_values vector of the last character's Node
-				return;
+	Node* curNode = m_root;
+	Node* prevNode = m_root;
+
+	if (m_root->m_children.size() != 0) { //Trie is not empty	
+		Pair* p = &(curNode->m_children.front());
+
+		while (i < key.size() && p != nullptr) {
+			if (p->label() == key[i]) { //found a Node with the right label, so move on to the next character 
+				i++;
+				if (i == key.size()) {//found the last character of key
+					p->child()->m_values.push_back(value); //push value to the m_values vector of the last character's Node
+					return;
+				}
+				else if (p->child() != nullptr) {
+					curNode = p->child(); //traverse deeper to the next Node
+					p = &(curNode->m_children.front());
+				}
+				else
+					break; //we have not reached the last character of key, but there are no more children
 			}
-			prev = j;
-			if (j->child() != nullptr)
-				j = j->child()->m_children.front(); //traverse down deeper to the next Node
 			else
-				break;
+				p++; //traverse across to other children of our current Node
 		}
-		else if (j->next != nullptr)
-			j = j->next; //traverse across other children of our current Node
-		else  //previously found a Node with the right label, but now the sequence is interrupted
-			break;
 	}
-	while (i < key.size() - 1) {
-		j = Pair(key[i], new Node());
-		j = j->child();
-		i++;
+	int j = key.size() - 1;
+	Node* newChild = nullptr;
+	Pair* newPair = nullptr;
+
+	while (j >= i) {
+		if (j == key.size() - 1)
+			newChild = new Node(value);
+		else 
+			newChild = new Node(*newPair);
+		newPair = new Pair(key[j], newChild);
+		j--;
 	}
-	j = Pair(key[i], new Node(value));
+	curNode->m_children.push_back(*newPair);
 	return;
-
-	//int k = key.size() - 1;
-	//Node lastNode(Pair(key[k], Node(value)));
-	//while (k > i) {
-	//	k--;
-	//	Node newNode(Pair(key[k], lastNode);
-	//	lastNode = newNode;
-	//}
-	//prev->m_children.push_back(key[i], newNode));
-
 }
 
-
+//template<typename ValueType>
+//void Trie<ValueType>::printMe(Node* current) {
+//	if (current == nullptr)
+//		return;
+//	Pair* p = &(current->m_children.front());
+//	while (p != nullptr) {
+//		std::cout << p->label();
+//		Pair* q = &(p->child()->m_children.front());
+//		while (q != nullptr) {
+//			return printMe(q->child());
+//			q++;
+//		}
+//		p++;
+//	}
+//
+//
+//}
+//
+//template<typename ValueType>
+//void Trie<ValueType>::print() {
+//	printMe(m_root);
+//}
 
 #endif // TRIE_INCLUDED
---
+++ Project4_EstherLi/Project4_EstherLi/main.cpp
@@ -0,0 +1,10 @@
+#include "Trie.h"
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	Trie<int> t1;
+	t1.insert("a", 1);
+	cout << "Passed all tests." << endl;
+}
